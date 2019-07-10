4883c6bc2909708a39e238e75b96c33b8b289e79
SoftMillsIOS <otaha@softmills.com>
2019-07-10 13:09:42 +0200

    commit

---
+++ Tamm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tamm
+//
+//  Created by apple on 7/9/19.
+//  Copyright © 2019 softmills.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
